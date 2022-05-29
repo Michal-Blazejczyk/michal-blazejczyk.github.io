d685ba49872606eaa9fe323c836ae4c9b3344f6a
Michal-Blazejczyk <64957284+Michal-Blazejczyk@users.noreply.github.com>
2022-05-29 08:24:53 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+/* import {HashRouter as Router} from 'react-router-dom'; */
+
+/* import AsideMenu from "./commponents/AsideMenu/AsiadeMenu"; */
+/* import Content from './commponents/Content/Content'; */
+/* import Header from "./commponents/header/Header"; */
+/* import StoreProvider from "./store/storeProvider"; */
+
+
+import './App.scss';
+
+const App = () => (
+
+    
+        <header>Witaj world</header>
+            
+           
+    
+);
+export default App;
